099c1402aeb2d9497c478649041cacd9f5f9beeb
Biniyam <114972065+bygxs@users.noreply.github.com>
2024-10-02 01:31:54 +0200

    Update page.tsx
    class="tracking-[0.5em] //in tailwind spaced-out word effect of CSS .spaced-word {
      letter-spacing: 0.5em;
    }

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* Header with Your Name */}
-      <header className="text-2xl font-bold row-start-1 tracking-wide">
+      <header className="text-2xl font-bold row-start-1 tracking-wide tracking-[0.5em]">
         {" "}
         B I N I Y A M{" "}
       </header>
